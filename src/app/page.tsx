6725f29e779a21df729d0397b33428597a3435cb
Win Cheng <winzamark12@gmail.com>
2024-03-18 13:55:07 -0700

    Trying out CI Dev workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="flex border border-black w-10 h-10"></div>
+            <div className="flex border border-black w-10 h-10"></div>
         </main>
     );
 }
