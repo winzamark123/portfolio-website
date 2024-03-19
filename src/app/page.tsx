561f60f60bd88cb5b002fe527d406ae1051686b6
Win Cheng <winzamark12@gmail.com>
2024-03-19 10:02:46 -0700

    Dark Theme with Toggle works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from './_components/ModeToggle';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex border border-black w-10 h-10"></div>
+      <ModeToggle />
+      <div className="flex border border-white w-10 h-10"></div>
       <div className="flex border border-blue-500 w-10 h-10"></div>
       <div className="flex border border-spacing-20 border-blue-800 w-32 h-32"></div>
     </main>
