31d1ece378133563f83e070f596bc3409d97bbd4
Win Cheng <winzamark12@gmail.com>
2024-03-19 13:07:15 -0700

    Min-Width 0 and some extra stuff

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -1,12 +1,27 @@
 import { ModeToggle } from './ModeToggle';
+import Link from 'next/link';
+
+interface NavLink {
+  name: string;
+  path: string;
+}
+
+const navLinks: NavLink[] = [
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about' },
+  { name: 'Experience', path: '/experience' },
+  { name: 'Projects', path: '/projects' },
+];
 
 export default function Nav() {
   return (
-    <nav className="fixed m-5">
+    <nav className="fixed m-5 flex h-4/5 flex-col justify-center rounded-xl border border-black dark:border-white">
       <ModeToggle />
-      <li>Home</li>
-      <li>Temp</li>
-      <li>About</li>
+      {navLinks.map((link) => (
+        <Link key={link.path} href={link.path} className="">
+          {link.name}
+        </Link>
+      ))}
     </nav>
   );
 }
