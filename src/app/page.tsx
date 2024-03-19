e6b780c529c69ad56e4be86467fc1f4db33e12fb
Win Cheng <winzamark12@gmail.com>
2024-03-18 23:04:03 -0700

    Tailwind added and fast refresh works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="flex border border-black w-10 h-10"></div>
-            <div className="flex border border-black w-10 h-10"></div>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex border border-black w-10 h-10"></div>
+      <div className="flex border border-blue-500 w-10 h-10"></div>
+      <div className="flex border border-spacing-20 border-blue-800 w-32 h-32"></div>
+    </main>
+  );
 }
