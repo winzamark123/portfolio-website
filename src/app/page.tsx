5e36950a7c83c730946a74d6add76aafb7890b20
Win Cheng <winzamark12@gmail.com>
2024-03-21 13:42:12 -0700

    Fixed Linting with Prettier plugins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import Projects from './_components/Projects/Projects';
 import Landing from './_components/Landing/Landing';
 export default function Home() {
   return (
-    <main className="p-1/10 pt-1/20 flex min-h-screen flex-col">
+    <main className="flex min-h-screen flex-col p-1/10 pt-1/20">
       <div className="flex h-96 w-full border border-white">
         <Landing />
       </div>
+      <div className="flex justify-normal p-2 pt-10"></div>
       <div className="flex h-96 w-full border border-blue-950">
         <Projects />
       </div>
