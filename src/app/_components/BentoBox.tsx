03b983d981c74d8cd54a188d2b330cea4eb2b917
Win Cheng <winzamark12@gmail.com>
2024-03-20 23:30:46 -0700

    Added Prettier Config and Some info on projects

--- src/app/_components/BentoBox.tsx
+++ src/app/_components/BentoBox.tsx
@@ -1,14 +1,68 @@
-import { cn } from '@/lib/utils';
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card';
+type Project = {
+  title: string;
+  description: string;
+  github_repo: string;
+  url?: string;
+};
+
+const Projects: [Project] = [
+  {
+    title: 'JustSayin',
+    description: 'Project 1 Description',
+    github_repo: '',
+  },
+  {
+    title: 'HackDavis Website',
+    description:
+      'Created a website for HackDavis 2024 to promote the event \
+    and provide information for attendees. The website had a traffic \
+    of 275,560 hits per month. With 8,300 unique visitors',
+    github_repo: '',
+    url: 'https://hackdavis.io/',
+  },
+  {
+    title: 'LStore Database',
+    description:
+      'Implemented a Lineage-based Data Store (LStore) \
+    which combines real-time processing of transactional and \
+    analytical workloads ran with concurrency using update-friendly lineage-based storage architecture',
+    github_repo: '',
+  },
+  {
+    title: 'DavisPNG',
+    description:
+      'A website marketplcae which matches student-photographers. \
+    with clients. Focused on providing a platform for students. ',
+    github_repo: 'https://github.com/winzamark123/DavisPNG_Frontend',
+    url: 'https://davispng.com/',
+  },
+];
 
 export default function BentoBox() {
   return (
-    <main
-      className={cn(
-        'flex min-h-screen flex-col items-center justify-between p-24'
-      )}
-    >
-      <div className={cn('flex border border-black w-10 h-10')}></div>
-      <div className={cn('flex border border-black w-10 h-10')}></div>
+    <main className="h-full w-full justify-normal border border-white">
+      <div className="h-full w-full bg-purple-500 p-20">
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import BentoBox from './_components/BentoBox';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex border border-white w-10 h-10"></div>
-      <div className="flex border border-blue-500 w-10 h-10"></div>
-      <div className="flex border border-spacing-20 border-blue-800 w-32 h-96"></div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        {'Button'}
-      </button>
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <div className="flex h-10 w-10 border border-white"></div>
+      <div className="flex h-10 w-10 border border-blue-500"></div>
+      <div className="flex h-96 w-32 border-spacing-20 border border-blue-800"></div>
+      <div className="flex h-96 w-full border border-blue-950">
+        <BentoBox />
+      </div>
     </main>
   );
 }
