ee11fedc58d5b97e5a922e3dbf7e40de682b647c
Win Cheng <winzamark12@gmail.com>
2024-03-23 10:55:35 -0700

    Added Logo

--- src/app/_components/Experience/Experience.tsx
+++ src/app/_components/Experience/Experience.tsx
@@ -1,3 +1,18 @@
+import React from 'react';
+import Rhombus_logo from '@public/logos/Rhombus.png';
+import Rhombus_bg from '@public/logos/Rhombus_back_logo.png';
+import HackDavis_logo from '@public/logos/HackDavis.svg';
+import HackDavis_bg from '@public/logos/HackDavis_back_logo.png';
+import Kebloom from '@public/logos/Kebloom.svg';
+import Kebloom_bg from '@public/logos/Kebloom_back_logo.png';
+import TASA from '@public/logos/TASA.png';
+import TASA_bg from '@public/logos/TASA_back_logo.png';
+import GDSC from '@public/logos/GDSC.svg';
+import GDSC_bg from '@public/logos/GDSC_back_logo.png';
+
+import Image from 'next/image';
+import type { StaticImageData } from 'next/image';
+
 import {
   Card,
   CardHeader,
@@ -6,14 +21,14 @@ import {
   CardDescription,
   CardContent,
 } from '@/components/ui/card';
-import React from 'react';
 
 type ExperienceProp = {
   company: string;
   position: string;
   description: string;
   date: string;
-//   icon: React.ReactNode;
+  icon: StaticImageData;
+  bg: StaticImageData;
 };
 
 const ExperienceProps: ExperienceProp[] = [
@@ -22,32 +37,43 @@ const ExperienceProps: ExperienceProp[] = [
     position: 'Software Engineer Intern',
     description: 'Description',
     date: 'August 2024 - Current',
+    icon: Rhombus_logo,
+    bg: Rhombus_bg,
   },
   {
     company: 'HackDavis',
     position: 'Technical Director',
     description: 'Description',
     date: 'September 2023 - Current',
+    icon: HackDavis_logo,
+    bg: HackDavis_bg,
   },
   {
     company: 'Kebloom',
     position: 'Software Engineer Intern',
     description: 'Description',
     date: 'June 2023 - August 2023',
+    icon: Kebloom,
+    bg: Kebloom_bg,
   },
   {
     company: 'Google Developer Student Club',
     position: 'Technical Lead',
     description: 'Description',
     date: 'January 2023 - June 2023',
+    icon: GDSC,
+    bg: GDSC_bg,
   },
   {
     company: 'TASA',
     position: 'Web Developer',
     description: 'Description',
     date: 'September 2022 - September 2023',
+    icon: TASA,
+    bg: TASA_bg,
   },
 ];
+
 export default function Experience() {
   return (
     <main className="flex w-full flex-col ">
@@ -64,6 +90,12 @@ export default function Experience() {
           >
             <CardHeader>
               <CardTitle>{experience.company}</CardTitle>
+              <Image
+                src={experience.icon}
+                alt={experience.company}
+                width={50}
+                height={50}
+              />
               <CardDescription>{experience.position}</CardDescription>
             </CardHeader>
             <CardContent>
