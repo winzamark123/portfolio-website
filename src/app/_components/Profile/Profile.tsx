30549f770b33edde1e11adf5c19e95498318d51b
Win Cheng <winzamark12@gmail.com>
2024-03-29 22:00:44 -0700

    Resizable Panels Implemented

--- src/app/_components/Profile/Profile.tsx
+++ src/app/_components/Profile/Profile.tsx
@@ -1,8 +1,12 @@
 import Myself from './_components/Myself';
+import Editor from './_components/Editor';
+
 export default function Profile() {
   return (
     <main className="flex h-full w-full">
-      <div className="flex w-2/3 border border-white"></div>
+      <div className="flex w-2/3 border border-white">
+        <Editor />
+      </div>
       <div className="flex w-1/3">
         <Myself />
       </div>
