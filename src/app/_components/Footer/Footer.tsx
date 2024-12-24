d26f70780fd741878d6becaa11886a8f6ffd176d
Win Cheng <winzamark12@gmail.com>
2024-12-24 11:44:32 +0800

    Added Tag on the homepage and nav back to home on the footer

--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -1,13 +1,17 @@
+import Link from 'next/link';
+
 export default function Footer() {
   const LineClass = 'flex-grow list-none h-0.5 bg-gray-500';
   return (
     <main className="flex h-third-screen w-full flex-col justify-center p-5">
       <div className="flex items-center justify-center ">
         <li className={LineClass} />
-        <div className="flex flex-col items-center justify-center p-5 text-3xl font-semibold">
-          <span>WIN CHENG'S</span>
-          <span>PORTFOLIO</span>
-        </div>
+        <Link href="/">
+          <div className="flex flex-col items-center justify-center p-5 text-3xl font-semibold">
+            <span>WIN CHENG'S</span>
+            <span>PORTFOLIO</span>
+          </div>
+        </Link>
         <li className={LineClass} />
       </div>
       <div className="flex items-center justify-center text-emerald-400">
