eaa270aa0e5f36788f710cf09036f443f9d385a0
Win Cheng <winzamark12@gmail.com>
2025-10-27 20:54:00 -0700

    update const

--- src/app/_components/const.tsx
+++ src/app/_components/const.tsx
@@ -126,7 +126,7 @@ export const ProjectList = [
     description:
       'bringing AI to your favorite whiteboard',
     github_repo: 'https://github.com/winzamark123/open-canvas',
-    url: 'https://open-canvas.vercel.app/',
+    url: 'https://www.opencanvas.studio/',
     tech_tags: ['typescript', 'ai sdk', 'monorepo', 'excalidraw'],
   },
   {
